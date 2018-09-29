27e22c3d85a9909fb9cd2ab07924c3e7db0b06a0
Dzmitry Bahadziazh <dima3911784@hotmail.com>
2018-09-29 08:56:49 +0300

    Implementation of makeExchange func

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    const coinValues = [50, 25, 10, 5, 1];
+    const coinSymbols = ['H', 'Q', 'D', 'N', 'P'];
+    const coinQuantity = [];
+    const exchangeObj = {};
+
+    if (currency <= 0) {
+        return {};
+    } else if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    } else {
+        const len = coinValues.length;
+        for (let i = 0; i < len; i++) {
+            if (coinValues[i] <= currency) {
+                coinQuantity.push( Math.floor(currency / coinValues[i]) );
+                if (coinQuantity[i] != 0) {
+                    exchangeObj[coinSymbols[i]] = coinQuantity[i];
+                }
+                currency -= (coinQuantity[i] * coinValues[i]);
+            } else {
+                coinQuantity.push(0);
+            }
+        }
+    }
+    return exchangeObj;
 }
